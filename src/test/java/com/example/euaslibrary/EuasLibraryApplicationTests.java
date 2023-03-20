1ae4437109a5ad54dce941fa70f96823a67ea624
Atanu Biswas <atanubiswas484@Atanus-MacBook-Air.local>
2023-03-20 14:11:00 +0200

    Initial commit

---
+++ src/test/java/com/example/euaslibrary/EuasLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.euaslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuasLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
